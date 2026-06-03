464a8f2b8b9520cf345ff7f66eed056c89b04851
Yasaschandrika <myc.yashu1@gmail.com>
2026-06-03 15:39:16 +0530

    fix keys

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ load_dotenv()
 app = Flask(__name__)
 
 API_KEYS = [
-    "AQ.Ab8RN6KhjnA3wEkj9r-kXN12nS0F1JXqjI1E0RPZDAjFLr_Iyg",
-    ""
-    "AQ.Ab8RN6KGc9cPB0CxXo8Uktl7wJ2Ba6yrVqLFXSndm0q_CZU-Sg"
+    os.getenv("GEMINI_API_KEY_1"),
+    os.getenv("GEMINI_API_KEY_2")
 ]
+
 
 import random
 client = genai.Client(api_key=random.choice(API_KEYS))
